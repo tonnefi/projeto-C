326c8a6c1aae0ff41e05f4afe5afde69700abbac
tonnefi <87247191+tonnefi@users.noreply.github.com>
2021-07-13 09:43:16 -0300

    Add files via upload

---
+++ pisca-pisca.ino
@@ -0,0 +1,25 @@
+
+// aqui estamos dizendo o que o arduino deve fazer ao ligar 
+void setup() 
+{
+  // definimos que o pino 3 do arduino vai mandar energia
+  pinMode(3, OUTPUT);
+}
+
+// aqui criamos um loop fazendo o código de repitir infinitamente
+void loop() 
+{
+  // ligamos o pino 3 a saida é de 5v  
+  digitalWrite(3, HIGH);
+
+  // estamos dando um delay de 50 milésimos de segundos
+  delay(50);
+
+  // Desligamos o pino 3
+  digitalWrite(3, LOW);
+
+  // estamos dando outro delay de 50 milésimos de segundos
+  delay(50);
+  
+  
+}
