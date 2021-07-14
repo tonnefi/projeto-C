c6ba59fa9ce271912de93ea5ca3b71a5a7d1be4c
tonnefi <87247191+tonnefi@users.noreply.github.com>
2021-07-14 12:05:17 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int A, B, X;
+    
+    printf("\ndigite um valor:");
+    scanf("%d", &A);
+    
+
+
+    printf("\ndigite um valor:");
+    scanf("%d", &B);
+
+
+
+    X = A + B;
+    
+    printf("\na soma de %d + %d é igual a %d.", A, B, X);
+    
+    return 0;
+}
+//                                              
